95c862a3de7e6398fe49bf4e529a3ed646b63124
Blaise Laflamme <blaise@laflamme.org>
2015-04-13 13:19:15 -0400

    add initial project files

---
+++ src/main.js
@@ -0,0 +1,3 @@
+'use strict';
+
+require('./sass/app.scss');
---
+++ src/vendors.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// jQuery
+require('jquery');
+
+// Bootstrap
+require('bootstrap');
+require('style!css!bootstrap/dist/css/bootstrap.min.css');
+
+// Font Awesome
+require('style!css!font-awesome/css/font-awesome.min.css');
+
+// Animate
+require('style!css!animate.css/animate.min.css');
