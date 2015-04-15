01fc2dda85f4134d35f1706287161c2744cb8a2d
Blaise Laflamme <blaise@laflamme.org>
2015-04-15 13:03:37 -0400

    first cut at highlight code

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
 require('./sass/app.scss');
+
+var hljs = require('highlight.js');
+
+hljs.initHighlightingOnLoad();
--- src/vendors.js
+++ src/vendors.js
@@ -12,3 +12,7 @@ require('style!css!font-awesome/css/font-awesome.min.css');
 
 // Animate
 require('style!css!animate.css/animate.min.css');
+
+// Highlight
+require('highlight.js');
+require('style!css!highlight.js/styles/tomorrow-night.css');
