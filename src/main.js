92f89f72bbe28157f611346114f3e93cde8b04af
Blaise Laflamme <blaise@laflamme.org>
2015-04-16 15:38:37 -0400

    show menu when hero section hides

--- src/main.js
+++ src/main.js
@@ -7,3 +7,12 @@ require('./img/pyramid-16x16.png');
 var hljs = require('highlight.js');
 
 hljs.initHighlightingOnLoad();
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > $('.hero-header').height()+130) {
+    $('nav#nav').addClass('show fadeIn animated');
+  }
+  else {
+    $('nav#nav').removeClass('show');
+  }
+});
