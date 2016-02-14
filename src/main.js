6d5735c63c1b159b68da915768d5ac7e86c89590
Steve Piercy <web@stevepiercy.com>
2016-02-14 01:31:25 -0800

    fix typo

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ require('./sass/app.scss');
 require('./img/pyramid-60x60.png');
 require('./img/pyramid-16x16.png');
 require('./img/linode.png');
-require('./img/substanced-d-logo.png');
+require('./img/substance-d-logo.png');
 
 var hljs = require('highlight.js');
 
