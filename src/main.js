8763b384e5b0808b796431bfde50eee857a5c336
Steve Piercy <web@stevepiercy.com>
2016-02-14 01:26:32 -0800

    add substance-d logo - fix bad html

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ require('./sass/app.scss');
 require('./img/pyramid-60x60.png');
 require('./img/pyramid-16x16.png');
 require('./img/linode.png');
+require('./img/substanced-d-logo.png');
 
 var hljs = require('highlight.js');
 
