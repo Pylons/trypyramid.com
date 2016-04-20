4d872da1ce2a6ca9a270b9c3c3e9e51f2a4536d7
Steve Piercy <web@stevepiercy.com>
2016-04-20 02:25:25 -0700

    - exclusive filter to work with bootstrap single toggle buttons - update maintainer for pyramid_authsanity

--- src/main.js
+++ src/main.js
@@ -32,12 +32,58 @@ var Isotope = require('isotope-layout');
 // make Isotope a jQuery plugin
 $.bridget( 'isotope', Isotope );
 // now you can use $().isotope()
+
+
+
+// basic usage
+
+// init Isotope
+// var $grid = $('.grid').isotope({
+//   // options
+// });
+// // filter items on button click
+// $('.filter-button-group').on( 'click', 'button', function() {
+//   var filterValue = $(this).attr('data-filter');
+//   $grid.isotope({ filter: filterValue });
+// });
+
+
+
+// inclusive selection
+
 // init Isotope
 var $grid = $('.grid').isotope({
-  // options
+  itemSelector: '.pyramid-item'
 });
-// filter items on button click
-$('.filter-button-group').on( 'click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
+
+// store filter for each group
+var filters = [];
+
+// change is-checked class on buttons
+$('.filters').on( 'click', 'button', function( event ) {
+  var $target = $( event.currentTarget );
+  $target.toggleClass('is-checked');
+  var isChecked = $target.hasClass('is-checked');
+  var filter = $target.attr('data-filter');
+  if ( isChecked ) {
+    addFilter( filter );
+  } else {
+    removeFilter( filter );
+  }
+  // filter isotope
+  // group filters together, inclusive
+  $grid.isotope({ filter: filters.join(',') });
 });
+
+function addFilter( filter ) {
+  if ( filters.indexOf( filter ) == -1 ) {
+    filters.push( filter );
+  }
+}
+
+function removeFilter( filter ) {
+  var index = filters.indexOf( filter);
+  if ( index != -1 ) {
+    filters.splice( index, 1 );
+  }
+}
